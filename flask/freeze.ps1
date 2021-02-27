3126483463dc4395437b7898f8ab23f522f15055
Fernando Ayats <ayatsfer@gmail.com>
2021-02-27 23:54:53 +0100

    uwsgi

--- flask/freeze.ps1
+++ flask/freeze.ps1
@@ -1,3 +1,4 @@
 & $PSScriptRoot\\env\\Scripts\\activate.ps1
 Set-Content -Path requirements.txt -Value (pip freeze | Select-String -Pattern "Flask-Uploads==" -NotMatch) 
 Add-Content -Path requirements.txt -Value "git+https://github.com/maxcountryman/flask-uploads.git"
+Add-Content -Path requirements.txt -Value "uwsgi"
