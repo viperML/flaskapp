203f7ac155dc6fb90035428a512c59beb6fda516
Fernando Ayats <ayatsfer@gmail.com>
2021-03-01 20:02:08 +0100

    js is hard

--- flask/portfolio/static/js/index.js
+++ flask/portfolio/static/js/index.js
@@ -1,13 +1,16 @@
-import { Remarkable } from 'remarkable';
-import * as mdb from 'mdb-ui-kit'; // lib
+import {Remarkable}  from 'remarkable';
+// Material Design Bootstrap dependencies
 import $ from "jquery";
 import '@popperjs/core';
 import 'bootstrap';
+import * as mdb from 'mdb-ui-kit';
+
 
 var md = new Remarkable();
 
 fetch("https://raw.githubusercontent.com/viperML/flaskapp/main/README.md")
 .then((response) => response.text())
 .then((html) => {
-    document.getElementById("DATA").innerHTML = md.render(html);
+    document.getElementById("README").innerHTML = md.render(html);
 })
+
--- flask/portfolio/static/webpack.config.js
+++ flask/portfolio/static/webpack.config.js
@@ -13,7 +13,11 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      }
+      },
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
+      },
     ]
-  },
+  }
 }
